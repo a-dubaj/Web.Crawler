23e32ec85b2e20a6be2069bec20ee8005616491d
a-dubaj <dubaj.andrzej@gmail.com>
2024-07-09 20:36:36 +0200

    feat: add prettier & tests

--- crawl.js
+++ crawl.js
@@ -1,12 +1,25 @@
-function normalizeURL(urlString){
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/'){
-        return hostPath.slice(0, -1)
-    }
-    return hostPath
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const linkElements = dom.window.document.querySelectorAll('a')
+  for (const linkElement of linkElements) {
+    urls.push(linkElement.href)
+  }
+  return urls
+}
+
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString)
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`
+  if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+    return hostPath.slice(0, -1)
+  }
+  return hostPath
 }
 
 module.exports = {
-    normalizeURL
-}   
+  normalizeURL,
+  getURLsFromHTML,
+}
--- crawl.test.js
+++ crawl.test.js
@@ -1,30 +1,43 @@
-const {normalizeURL} = require('./crawl.js')
-const {test, expect} = require('@jest/globals')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
-    const input = 'https://blog.boot.dev/path'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
+  const input = 'https://blog.boot.dev/path'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
 })
 
 test('normalizeURL strip trailing slash', () => {
-    const input = 'https://blog.boot.dev/path'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
+  const input = 'https://blog.boot.dev/path'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL capitals', ()=>{
-    const input = 'https://BLOG.boot.dev/path/'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
+test('normalizeURL capitals', () => {
+  const input = 'https://BLOG.boot.dev/path/'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL strip http', ()=>{
-    const input = 'http://blog.boot.dev/path/'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
+test('normalizeURL strip http', () => {
+  const input = 'http://blog.boot.dev/path/'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML', () => {
+  const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        </body>
+    </html>`
+  const inputBaseURL = 'https://blog.boot.dev'
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ['https://blog.boot.dev/']
+  expect(actual).toEqual(expected)
 })
